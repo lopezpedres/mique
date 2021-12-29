3a34907ef239e6519c5afbee9a52d855a6e801d1
Miguel Lopez Pedres <lopezpedres@gmail.com>
2021-12-29 12:04:39 -0800

    Create Welcome Component JSX

---
+++ src/Pages/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Welcome = () => {
+    return (
+        <container id="#Welcome_Section">
+            <div className="row">
+                <div className=" welcomeText column"> 
+                <h1>Hi</h1>
+                <h1>there</h1>
+                </div>
+                <div className="column"> 2</div>
+
+            </div>
+
+           
+        </container>
+    )
+}
+
+export default Welcome
