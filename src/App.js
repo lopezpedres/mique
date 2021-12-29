b56073db08fee612f074eac9748eecd46b84d250
Miguel Lopez Pedres <lopezpedres@gmail.com>
2021-12-29 12:04:14 -0800

    Add Welcome Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import NavBar from './Components/NavBar';
+import Welcome from './Pages/Welcome';
 
 function App() {
   return (
     <div className="App">
     <NavBar />
+    <Welcome/>
     </div>
   );
 }
