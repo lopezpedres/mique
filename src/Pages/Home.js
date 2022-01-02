3afab2c9da4335037571167b982689b9ae9d276b
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-01-01 22:20:46 -0800

    Add EmojiApi to Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import heatherImage from "../assets/images/pp.jpg"
 
-const emoji = "👽"
 const styleHigh={ margin: "0px",
 }
+const initialEmoji = ""
 
 const Home = () => {
+    const [emoji, setEmoji] = useState(initialEmoji)
+    useEffect(() => {
+        async function getEmoji() {
+        const bodyParams = {"limit": 1}
+        const params = new URLSearchParams(bodyParams)
+        const url = "https://api.emojisworld.fr/v1/random?"+params
+        const response = await fetch(url)
+        var data = await response.json()
+        console.log(data)
+        const {emoji}=data.results[0]
+        console.log("This is the emoji:"+ emoji)
+        setEmoji(emoji)
+        }
+        getEmoji()
+    }
+    ,[])
+    console.log(emoji)
     return (
         <div id="#Welcome_Section" className='Col-container'>
             <div className="Row">
