8deef554d4b8a37a07431730f7bfd7d3258e00ff
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-01-01 22:20:28 -0800

    Set Emoji in Home

--- src/Layouts/Layout.js
+++ src/Layouts/Layout.js
@@ -3,14 +3,19 @@ import NavBar from "../Components/NavBar";
 import About from "../Pages/About";
 import Home from "../Pages/Home";
 import Projects from "../Pages/Projects";
+import Emoji from "../apis/Emoji";
+
+
+
+
 
 
 const Layout = () => {
   return (
     <div>
       <NavBar/>
-      <Home/>
-      <Projects/>
+      <Home emoji={Emoji}/>
+      <Projects />
       <About/>
 
     </div>
