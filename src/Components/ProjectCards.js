5a60602b9e7cad2e7ba6cca0f1fed046e998bffb
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-01-05 11:54:55 -0800

    Created shoState for the Modal

--- src/Components/ProjectCards.js
+++ src/Components/ProjectCards.js
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import heatherImage from "../assets/images/pp.jpg"
+import Modal from './Modal';
 
 
+const onShow={display:"block"}
 
 
 
 const ProjectCards = ({project}) => {
+    const [show, setShow] = useState(null)
+
+    const toggleModal = ()=>{
+        setShow(onShow)
+    
+    }
     return (
        
         
-            <div className="Column">
-                 <div className="ProjectCards">
-                 <img src={heatherImage}/>
-                  <p>{project.name}</p>
+                
+            <div className="Column" >
 
+                 <div className="ProjectCards" onClick={()=>toggleModal()}>
+                 <img alt="Miguel Lopez"src={heatherImage}/>
+                  <p>{project.name}</p>
+                  
+                  
                  </div>
-            
+
+                 <Modal show={show} setShow={setShow}/>
+                 
+
            
         </div>
+                
        
    
     )
