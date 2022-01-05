3a71289a94174a04c6e8ee81c24ebcd64a902655
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-01-04 19:44:11 -0800

    Isolate the emoji API script. Changed the name of the title

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,37 +4,26 @@ import getEmoji from '../apis/Emoji'
 
 const styleHigh={ margin: "0px",
 }
+const styleHigh2={ margin: "0px",
+fontWeight: "normal"
+
+}
 const initialEmoji = "🍄"
 
 const Home = () => {
     const [emoji, setEmoji] = useState(initialEmoji)
-    // async function getEmoji() {
-    //     const bodyParams = {"limit": 1}
-    //     const params = new URLSearchParams(bodyParams)
-    //     const url = "https://api.emojisworld.fr/v1/random?"+params
-    //     const response = await fetch(url)
-    //     var data = await response.json()
-    //     console.log(data)
-    //     const {emoji}=data.results[0]
-    //     console.log("This is the emoji:"+ emoji)
-    //     setEmoji(emoji)
-    //     }
-    const emojiToggle = (equis)=>{
-        setTimeout(()=>equis(setEmoji),3000)
-
-    }
     useEffect(() => {
-        emojiToggle(getEmoji)
+        setTimeout(()=>getEmoji(setEmoji),3000)
        
     }
-    ,[emoji])
+    ,[])
     return (
         <div className='Col-container'>
             <div className="Row">
                 <div className=" welcomeText Column">
                     <p>Hi there  {emoji}</p>
                     <h1 style={styleHigh}>I'm Miguel </h1>
-                    <p style={styleHigh}>A Software Engineer </p>
+                    <p style={styleHigh2}>Full-Stack Developer </p>
                     <div className="Rectangulo"></div>
                 </div>
                 <div className="Column">
