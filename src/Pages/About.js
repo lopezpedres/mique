8401dc3e5b0e343232db5bfb6fb2e72d38fa73d2
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-01-08 20:49:53 -0800

    Typos

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className="card">
+        <div id="about_me" className="card">
             <h1 className="newTitleBlock">About me</h1>
             <p>
-            Welcome to my website. My name is Miguel and I am a full-stack Software Developer. For the past 2 years of self-learning, I have been able to put in practice many technologies, such as JavaScript, Python, SQL, SQLALCHEMY, Postgresql, Express, Docker, React, and more. Currently, and for the past few months, I have been part of Hack for LA Developer’s team, a non-profit organization which principal objective is to give back to the community by creating tech solutions to everyday problems. There, I am able to give maintenance to their principal WebPage as well as learn more about GitHub’s Kanban workflow. 
+                Welcome to my website. My name is Miguel and I am a Full-Stack Software Developer. For the past 2 years of self-learning, I have been able to put in practice many technologies, such as JavaScript, Python, SQL, SQLALCHEMY, Postgresql, Express, Docker, React, and more. Currently, and for the past few months, I have been part of Hack for LA Developer’s team, a non-profit organization which principal objective is to give back by creating tech solutions to everyday problems of the community. There, I am able to give maintenance to their principal WebPage as well as learn more about GitHub’s Kanban workflow.
             </p>
-            
+
         </div>
     )
 }
