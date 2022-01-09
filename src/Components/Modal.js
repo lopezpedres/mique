b3876f6be08abec66381a866252f7f7865b34dc3
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-01-08 20:49:01 -0800

    Created Modal Content

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,18 +1,33 @@
 
 
 
-const exitOn={display:"none"}
+
+const exitOn = { display: "none" }
 
 
-const Modal = ({show, setShow}) => {
- 
+const Modal = ({ project, show, setShow }) => {
+
     return (
-        <div id="myModal" style={show} className="Modal">
+        <div id="myModal" style={ show } className="Modal">
             <div className='ModalContent'>
-                Hi Modal
-            <span className="close" onClick={()=>setShow(exitOn)}>&times;</span>
+                <h1>{ project.name }</h1>
+                <i style={{padding:"5px"}}className="fa fa-video-camera fa-2x" aria-hidden="true"> <span style={{fontFamily:"Roboto", fontSize:"20px"}}>Demo</span></i>
+                
+                <i style={{padding:"5px"}} className="fa fa-github fa-2x" aria-hidden="true"><span>Repository</span></i>
+                <h3>Description</h3>
+                <p>{project.description}</p>
+                
+                <h3><i className="fa fa-slack size" aria-hidden="true"></i>Stack</h3>
+                <ul>
+
+                    { project.tech.map(eachProject => (
+
+                        <p style={{textAlign:"left"}}>  { eachProject } </p>
+                    )) }
+                </ul>
+                <span className="close" onClick={ () => setShow(exitOn) }>&times;</span>
             </div>
-           
+
         </div>
     )
 }
