ea77062d21497769fcfd649c527d42e90d795ef5
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-07-15 13:24:14 -0700

    Website update banner

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,10 +3,12 @@ import heatherImage from "../assets/images/yo.jpg"
 // import getEmoji from '../apis/Emoji'
 import resume from "../assets/pdfs/resume.pdf"
 
-const styleHigh={ margin: "0px", fontSize:"95px"
+const styleHigh = {
+    margin: "0px", fontSize: "95px"
 }
-const styleHigh2={ margin: "0px",
-fontWeight: "normal"
+const styleHigh2 = {
+    margin: "0px",
+    fontWeight: "normal"
 
 }
 const initialEmoji = "🔅"
@@ -16,22 +18,23 @@ const Home = () => {
     // const [emoji, setEmoji] = useState(initialEmoji)
     // useEffect(() => {
     //     setTimeout(()=>getEmoji(setEmoji),3000)
-       
+
     // }
     // ,[])
     return (
         <div className='Col-container'>
+            <div style={ { marginTop: "5rem", textAlign: "center", backgroundColor: "#F17A7E", padding: "10px", fontSize: "20px", fontFamily: "Raleway" } }>This website is being updated with new cool projects</div>
             <div className="Row">
                 <div className=" welcomeText Column">
-                    <p>Hi there  {initialEmoji}</p>
-                    <p style={styleHigh}>I'm Miguel </p>
-                    <p style={styleHigh2}>Full-Stack Developer </p>
+                    <p>Hi there  { initialEmoji }</p>
+                    <p style={ styleHigh }>I'm Miguel </p>
+                    <p style={ styleHigh2 }>Full-Stack Developer </p>
                     <div className="Rectangulo"></div>
-                    <a href={resume} className="button">Resume</a>
+                    <a href={ resume } className="button">Resume</a>
                 </div>
                 <div className="Column">
-                    <img className="welcomImg" alt = "Miguel Lopez"src={heatherImage}/>
-                    </div>
+                    <img className="welcomImg" alt="Miguel Lopez" src={ heatherImage } />
+                </div>
 
             </div>
 
